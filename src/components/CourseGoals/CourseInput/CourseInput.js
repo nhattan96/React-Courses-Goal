0df35ecc94ebd5323986ff14f4c263e5634d7234
nhattan96 <tranhuynhnhattan96@gmail.com>
2022-04-06 23:35:38 +0700

    update

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,72 @@
 import React, { useState } from 'react';
+// import styled from 'styled-components'
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+// import './CourseInput.css';
+
+import styles from './CourseInput.module.css'
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+// }
+
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc ${props.invalid ? 'red' : '#ccc'};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+
+// &.invalid input {
+//   border-color: red;
+//   background-color: #ffd7d7;
+// }
+
+// &.invalid label {
+//   color: red
+// }
+// `
+
+// Use styled-component
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.invalid ? 'red' : 'black'}
+// }
+
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc ${props => (props.invalid ? 'red' : '#ccc')};
+//   background: ${props => (props.invalid ? '#fad0ec' : 'transparent')};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+// `
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -26,10 +91,25 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
+  // use styled-component
+  // return (
+  //   <form onSubmit={formSubmitHandler}>
+  //     {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`} > */}
+  //     {/* <FormControl className={!isValid && 'invalid'}> */}
+  //     <FormControl invalid={!isValid}>
+  //       <label>Course Goal</label>
+  //       <input type="text" onChange={goalInputChangeHandler} />
+  //     </FormControl>
+  //     {/* </FormControl> */}
+  //     {/* </div> */}
+  //     <Button type="submit">Add Goal</Button>
+  //   </form>
+  // );
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`} >
-        <label className={'form-control inv'}>Course Goal</label>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+        <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
